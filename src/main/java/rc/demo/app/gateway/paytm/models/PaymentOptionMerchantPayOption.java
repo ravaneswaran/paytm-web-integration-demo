ed53de5306a8a099042ee229b3844dbd19efb589
Ravaneswaran Chinnasamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-10-21 13:13:13 +0530

    PaymentOptionMerchantPayOption.java updated

--- src/main/java/rc/demo/app/gateway/paytm/models/PaymentOptionMerchantPayOption.java
+++ src/main/java/rc/demo/app/gateway/paytm/models/PaymentOptionMerchantPayOption.java
@@ -11,7 +11,36 @@ public class PaymentOptionMerchantPayOption {
 	private String userProfileSarvata;
 	
 	private String activeSubscriptions;
-	
-	
 
+	public List<PaymentOptionPaymentMode> getPaymentOptionPaymentMode() {
+		return paymentOptionPaymentMode;
+	}
+
+	public void setPaymentOptionPaymentMode(List<PaymentOptionPaymentMode> paymentOptionPaymentMode) {
+		this.paymentOptionPaymentMode = paymentOptionPaymentMode;
+	}
+
+	public List<PaymentOptionSavedInstruments> getPaymentOptionSavedInstruments() {
+		return paymentOptionSavedInstruments;
+	}
+
+	public void setPaymentOptionSavedInstruments(List<PaymentOptionSavedInstruments> paymentOptionSavedInstruments) {
+		this.paymentOptionSavedInstruments = paymentOptionSavedInstruments;
+	}
+
+	public String getUserProfileSarvata() {
+		return userProfileSarvata;
+	}
+
+	public void setUserProfileSarvata(String userProfileSarvata) {
+		this.userProfileSarvata = userProfileSarvata;
+	}
+
+	public String getActiveSubscriptions() {
+		return activeSubscriptions;
+	}
+
+	public void setActiveSubscriptions(String activeSubscriptions) {
+		this.activeSubscriptions = activeSubscriptions;
+	}
 }
