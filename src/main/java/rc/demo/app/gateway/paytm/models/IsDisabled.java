c422c0e2baba6ef5e43bd7f9a178a1fecbf7c875
Ravaneswaran Chinnasamy <ravaneswaran.chinnasamy@niivsoftware.com>
2019-10-21 13:08:00 +0530

    IsDisabled.java updated

--- src/main/java/rc/demo/app/gateway/paytm/models/IsDisabled.java
+++ src/main/java/rc/demo/app/gateway/paytm/models/IsDisabled.java
@@ -2,4 +2,43 @@ package rc.demo.app.gateway.paytm.models;
 
 public class IsDisabled {
 
+	private String status;
+
+	private String message;
+
+	private String userAccountExist;
+
+	private String merchantAccept;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUserAccountExist() {
+		return userAccountExist;
+	}
+
+	public void setUserAccountExist(String userAccountExist) {
+		this.userAccountExist = userAccountExist;
+	}
+
+	public String getMerchantAccept() {
+		return merchantAccept;
+	}
+
+	public void setMerchantAccept(String merchantAccept) {
+		this.merchantAccept = merchantAccept;
+	}
 }
